f1cf7ca85a27064be98f40bbf744f71132cb438e
Zachary Astree <zacharyastree@gmail.com>
2021-07-28 10:26:22 -0400

    Mobile integration

--- DeployDocker/app/Http/Controllers/PostsController.php
+++ DeployDocker/app/Http/Controllers/PostsController.php
@@ -2,47 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post = Post::all();
+
+        return view('posts.index',[
+            
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('posts.create');
     }
 
-    public function store()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        $data = request()->validate([
-            'caption' => 'required',
-            'title' => 'required',
-            'ingredients' => 'required',
-            'instructions' => 'required',
-            'servings' => 'required',
-            'c_time' => 'required',
-            'p_time' => 'required',
-            'notes' => 'required',
-            'image' => 'required|image',
-        ]);
-
-        $imagePath = request('image')->store('uploads','public');
-
-        auth()->user()->posts()->create([
-            'caption' => $data['caption'],
-            'title' => $data['title'],
-            'ingredients' => $data['ingredients'],
-            'instructions' => $data['instructions'],
-            'servings' => $data['servings'],
-            'c_time' => $data['c_time'],
-            'p_time' => $data['p_time'],
-            'notes' => $data['notes'],
-            'image' => $imagePath,
-        ]);
-       
-
-        return redirect('/profile/' . auth()->user()->id);
+        {
+            $data = request()->validate([
+                'caption' => 'required',
+                'title' => 'required',
+                'ingredients' => 'required',
+                'instructions' => 'required',
+                'servings' => 'required',
+                'c_time' => 'required',
+                'p_time' => 'required',
+                'notes' => 'required',
+                'image' => 'required|image',
+            ]);
+    
+            $imagePath = request('image')->store('uploads','public');
+    
+            auth()->user()->posts()->create([
+                'caption' => $data['caption'],
+                'title' => $data['title'],
+                'ingredients' => $data['ingredients'],
+                'instructions' => $data['instructions'],
+                'servings' => $data['servings'],
+                'c_time' => $data['c_time'],
+                'p_time' => $data['p_time'],
+                'notes' => $data['notes'],
+                'image' => $imagePath,
+            ]);
+           
+    
+            return redirect('/profile/' . auth()->user()->id);
+        }
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- DeployDocker/resources/views/profiles/index.blade.php
+++ DeployDocker/resources/views/profiles/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-5">
+        <div class="col-lg-3 p-5">
             <img src="{{ asset('img/cham.jpg') }}" alt="" class="rounded-circle" style="height:150px;">
         </div>
-        <div class="col-9 pt-5">
+        <div class="col-md-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <div>
                     <h1>{{ $user->name }}</h1>
@@ -21,27 +21,10 @@
     </div>
 
     <div class="row pt-5">
-
         @foreach($user->posts as $post)
-        <div class="col-4 pb-4">
-            <img src="/storage/{{ $post->image }}" class="w-100" style="height:350px; width:250px;" alt="">
-
-            <p> <b>Title:</b> {{ $post->title }} </p>
-
-            <p> <b>Description:</b> {{ $post->caption }} </p>            
-
-            <p> <b>Servings:</b> {{ $post->servings }} </p>
-
-            <p> <b>Cooking Time:</b> {{ $post->c_time }} minutes</p>
-
-            <p> <b>Prep Time:</b> {{ $post->p_time }} minutes</p>
-
-            <p> <b>Ingredients:</b> {{ $post->ingredients }} </p>
-
-            <p> <b>Instructions:</b> {{ $post->instructions }} </p>
-
-            <p> <b>Notes:</b> {{ $post->notes }} </p>
-        </div>
+            <div class="col-md-4 pb-4">
+                @include('layouts/postbuild')
+            </div>
         @endforeach
     </div>
     @endsection
