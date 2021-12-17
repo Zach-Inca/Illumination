a261cab30a304ff9146e01bbf4e9e253aae24e86
Jakhongir Bekchanov <bekchanovj@gmail.com>
2021-12-17 15:44:47 -0500

    testing chatbot

--- DeployDocker/resources/views/profiles/index.blade.php
+++ DeployDocker/resources/views/profiles/index.blade.php
@@ -44,6 +44,26 @@
         </div>
     </div>
 
+    <script src="https://d2t7inh1nap6m2.cloudfront.net/lex-web-ui-loader.min.js"></script>
+    <script>
+    var loaderOpts = {
+        baseUrl: 'https://d2t7inh1nap6m2.cloudfront.net/',
+        shouldLoadMinDeps: true
+    };
+    var loader = new ChatBotUiLoader.IframeLoader(loaderOpts);
+    var chatbotUiConfig = {
+            /* Example of setting session attributes on parent page
+            lex: {
+                sessionAttributes: {
+                userAgent: navigator.userAgent,
+                QNAClientFilter: ''
+                }
+            }
+            */
+            };
+    loader.load(chatbotUiConfig)
+        .catch(function (error) { console.error(error); });
+    </script>
 
     <div class="row pt-5">
         @foreach($user->posts as $post)
